b287c7d4c37a11400b3cbaa8800192583c2b8da3
jessy <rugurujane1@gmail.com>
2025-04-04 00:09:01 +0300

    Updated index.html and App.jsx, replaced asset file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CartOverlay from "./components/CartOverlay";
 import { CartProvider, useCart } from "./context/CartContext";
 import ProductListingPage from "./components/ProductListingPage";
@@ -31,9 +31,7 @@ const AppContent = () => {
 function App() {
   return (
     <CartProvider>
-      <Router>
-        <AppContent />
-      </Router>
+      <AppContent />
     </CartProvider>
   );
 }
